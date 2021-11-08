8da39fa179fb5d7332c1c9aee5b2803c4f393ae8
lfernandes25 <leanstorm@hotmail.com>
2021-11-08 01:25:50 -0300

    CAPY003 - Insere busca de elementos e maximiza o navegador

---
+++ tests/features/specs/buscando_elementos.feature
@@ -0,0 +1,9 @@
+# language: pt
+
+@CAPY003
+Funcionalidade: Buscar elementos
+
+Cenário: Buscar elementos com sucesso
+Dado que esteja no navegador
+Quando acesso a url de botoes
+Então verifico se encontrei os elementos
--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -3,7 +3,15 @@ require "selenium-webdriver"
 
 Capybara.configure do |config|
   #selenium selenium_chrome selenium_chrome_healess
-  config.default_driver = :selenium_chrome
+  config.default_driver = :selenium
   config.app_host = "https://automacaocombatista.herokuapp.com"
   config.default_max_wait_time = 10 # espera 10 segundos
+end
+
+#Configura para maximizar o navegador
+Capybara.register_driver :selenium do |app|
+  option = ::Selenium::WebDriver::Chrome::Options.new
+  option.args << "--start-maximized"
+  option.args << "--disable-infobars"
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: option)
 end
